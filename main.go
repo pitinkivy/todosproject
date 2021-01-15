c1c745f0c2e50b1da136e66b91981a1db673d2a7
Pallat Anchaleechamaikorn <yod.pallat@gmail.com>
2021-01-15 20:36:49 +0700

    refactor group

--- main.go
+++ main.go
@@ -6,72 +6,64 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
-	"strings"
 
 	echo "github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
-	"github.com/spf13/viper"
 	"github.com/joho/godotenv"
-	"github.com/pallat/todos/captcha"
 	"github.com/pallat/todos/auth"
-	"github.com/pallat/todos/todos"
+	"github.com/pallat/todos/captcha"
 	"github.com/pallat/todos/logger"
+	"github.com/pallat/todos/todos"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
-)
-
-import (
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
-  )
+)
 
 func goDotEnvVariable(key string) string {
 
 	// load .env file
 	err := godotenv.Load(".env")
-  
+
 	if err != nil {
-	  log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file")
 	}
-  
+
 	return os.Getenv(key)
 }
 
 func main() {
 	//goDotEnvVariable()
 
-	
-
-
 	viper.SetDefault("app.addr", "0.0.0.0:8888")
-	
+
 	viper.AddConfigPath(".")
 	//viper.SetConfigFile(".env")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".","_"))
-	
-	
-	if err:=viper.ReadInConfig();err != nil{
-		log.Println("Warning:",err);
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println("Warning:", err)
 	}
-	
+
 	l, _ := zap.NewProduction()
 	defer l.Sync() // flushes buffer, if any
-
 
 	// github.com/denisenkom/go-mssqldb
 	dsn := viper.GetString("app.db_connection")
 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
-	if(err != nil){
-		log.Fatal(err);
+	if err != nil {
+		log.Fatal(err)
 		/*
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"err":err.Error(),
-		})
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"err":err.Error(),
+			})
 		*/
 	}
 
@@ -92,17 +84,13 @@ func main() {
 	router.POST("/exchange", exchangeHandler)
 	//#endregion
 
+	todosGroup := router.Group("/todos")
+	todosGroup.Use(auth.MiddlewareJwtAuthen())
 	//#region secure path using Bearer
-	router.POST("/todos", todos.NewTodoHandler(db))
-	router.GET("/todos", todos.GetTodoHandler(db))
-	router.PUT("/todos/:id", todos.PutUpdateTodoHandler(db))
-	router.DELETE("/todos/:id", todos.DeleteTodoHandler(db))
-	
-	g := router.Group("/todos")
-	//_ = g
-	g.Use(auth.MiddlewareJwtAuthen())
-	//#endregion
-
+	todosGroup.POST("/", todos.NewTodoHandler(db))
+	todosGroup.GET("/", todos.GetTodoHandler(db))
+	todosGroup.PUT("/:id", todos.PutUpdateTodoHandler(db))
+	todosGroup.DELETE("/:id", todos.DeleteTodoHandler(db))
 
 	srv := &http.Server{
 		Addr:         viper.GetString("app.addr"),
@@ -112,14 +100,13 @@ func main() {
 	}
 
 	go func() {
-		log.Printf("serve on: %s\n",viper.GetString("app.addr"))
+		log.Printf("serve on: %s\n", viper.GetString("app.addr"))
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
 	quit := make(chan os.Signal)
-
 
 	// (syscall.SIGINT)ctrl + c or (syscall.SIGTERM)kill
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
@@ -128,13 +115,13 @@ func main() {
 
 	// wait 5 second
 	/*
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown:", err)
-	}
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatal("Server forced to shutdown:", err)
+		}
 	*/
-	
+
 	// wait until job done
 	if err := srv.Shutdown(context.Background()); err != nil {
 		log.Fatal("Server forced to shutdown:", err)
@@ -143,50 +130,48 @@ func main() {
 	log.Println("Server exiting")
 }
 
-
-func captchaHandler (c echo.Context) error{
-	key, captcha := captcha.KeyQuestion();
+func captchaHandler(c echo.Context) error {
+	key, captcha := captcha.KeyQuestion()
 	return c.JSON(http.StatusOK, map[string]string{
-		"key":key,
-		"captcha":captcha,
+		"key":     key,
+		"captcha": captcha,
 	})
 }
 
-func exchangeHandler(c echo.Context) error{
+func exchangeHandler(c echo.Context) error {
 	type Answer struct {
-		Key string `json:"key"`
-		Answer int `json:"answer"`
+		Key    string `json:"key"`
+		Answer int    `json:"answer"`
 	}
 
 	var ans Answer
 	if err := c.Bind(&ans); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error":err.Error(),
+			"error": err.Error(),
 		})
 	}
 
-	if !captcha.Answer(ans.Key, ans.Answer){
+	if !captcha.Answer(ans.Key, ans.Answer) {
 		return c.JSON(http.StatusUnauthorized, map[string]string{
-			"error":"wrong answer",
+			"error": "wrong answer",
 		})
 	}
 
-	t, err :=  auth.Token()
-	if err != nil{
+	t, err := auth.Token()
+	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error":err.Error(),
+			"error": err.Error(),
 		})
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{
-		"token":t,
+		"token": t,
 	})
 
-
 	/*
-	key, captcha := captcha.KeyQuestion();
-	return c.JSON(http.StatusOK, map[string]string{
-		"key":key,
-		"captcha":captcha,
-	})*/
+		key, captcha := captcha.KeyQuestion();
+		return c.JSON(http.StatusOK, map[string]string{
+			"key":key,
+			"captcha":captcha,
+		})*/
 }
